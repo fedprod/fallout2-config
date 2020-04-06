2be9919ae2150d39555e90a00179f7f47b7844cb
fedprod <fedprod@users.noreply.github.com>
2020-04-06 08:26:42 +0200

    Update AssemblyInfo.cs

--- fo2-config/Properties/AssemblyInfo.cs
+++ fo2-config/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fallout 2 config")]
+[assembly: AssemblyTitle("Fallout 2 Config")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rotators")]
-[assembly: AssemblyProduct("Fallout 2 config")]
+[assembly: AssemblyProduct("Fallout 2 Config")]
 [assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
